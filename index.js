c5fcbb386580e32485090b047aefa64505bbdec0
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-06-26 22:10:10 -0400

    added code to generate svg string based on prompt results

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
 // Prompts the user for questions about their desired logo.
 function userPrompt() {
@@ -9,6 +10,12 @@ function userPrompt() {
             type: 'input',
             name: 'text',
             message: "Enter text for your graphic, up to three characters.",
+            validate: (answer) => {
+                if (answer.length > 3) {
+                    return console.log("\n Text must be three characters or less! Please try again");
+                }
+                return true;
+            }
         },
         {
             type: 'input',
@@ -29,6 +36,37 @@ function userPrompt() {
      ])
      .then((answers) => {
        //Generate svg logo based on answers
+       let shape;
+       switch (answers.shape) {
+        case 'Circle':
+            shape = new Circle();
+            break;
+        case 'Triangle':
+            shape = new Triangle();
+            break;
+        case 'Square':
+            shape = new Square();
+            break;
+       }
+
+       shape.setColor(answers.shapeColor);
+
+       const svgString = 
+       `<svg version="1.1"
+            width="300" height="200"
+            xmlns="http://www.w3.org/2000/svg">
+            ${shape.render()}
+            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}
+        </svg>`
+
+
+       fs.writeFile('logo.svg', svgString, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('SVG logo generated successfully!');
+        }
+    });
      });
 }
 
