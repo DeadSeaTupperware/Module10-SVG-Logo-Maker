69acacd7920133b11af66a6b142cea0499757377
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-06-26 22:36:52 -0400

    adjusted shapes.js shape sizes

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function userPrompt() {
             width="300" height="200"
             xmlns="http://www.w3.org/2000/svg">
             ${shape.render()}
-            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}
+            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
         </svg>`
 
 
